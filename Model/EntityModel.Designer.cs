9a470d39282bf127393bfcbbef9b3f6025c4fc95
beni2704 <benigarcia853@gmail.com>
2022-06-15 20:58:06 +0700

    add logo and data

--- Model/EntityModel.Designer.cs
+++ Model/EntityModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ashbo\source\repos\PSDProject\PSDProject\Model\EntityModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\D\tugas kul\psd\PSDProject\Model\EntityModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Views/Template.Master.designer.cs
+++ Views/Template.Master.designer.cs
@@ -42,6 +42,15 @@ namespace PSDProject
         protected global::System.Web.UI.WebControls.HyperLink HomeIcon;
 
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
         /// ManagePanel control.
         /// </summary>
         /// <remarks>
