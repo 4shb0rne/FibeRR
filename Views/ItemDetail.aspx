2c1c7089d20baea07368b045029fc6ec1bf90afe
beni2704 <benigarcia853@gmail.com>
2022-06-16 16:23:30 +0700

    fix item detail & nav

--- Views/ItemDetail.aspx
+++ Views/ItemDetail.aspx
@@ -5,8 +5,8 @@
      <div style="margin: 2% 0% ">
         <div class="d-flex justify-content-around ">
             <div class="flex-left-gig">
-                <h1><asp:Label ID="CategoryLbl" runat="server"></asp:Label></h1>
-                <h3><asp:Label ID="ClothLbl" runat="server"></asp:Label></h3>
+                <h1><asp:Label ID="ClothLbl" runat="server"></asp:Label></h1>
+                <h3><asp:Label ID="CategoryLbl" runat="server"></asp:Label></h3>
                 <asp:Image ID="Image" runat="server" class="card-img-top title-hover" style="max-width:500px;"/>
                 <h5 class="mt-3">Detail : </h5>
                 <p><asp:Label ID="DescLbl" runat="server"></asp:Label></p>
--- Views/ItemDetail.aspx.designer.cs
+++ Views/ItemDetail.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace PSDProject.Views
     {
 
         /// <summary>
-        /// CategoryLbl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label CategoryLbl;
-
-        /// <summary>
         /// ClothLbl control.
         /// </summary>
         /// <remarks>
@@ -31,6 +22,15 @@ namespace PSDProject.Views
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label ClothLbl;
+
+        /// <summary>
+        /// CategoryLbl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label CategoryLbl;
 
         /// <summary>
         /// Image control.
